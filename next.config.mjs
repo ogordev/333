6e56b66767f720d7939ae6817ac81ac0c5cd99d7
ogordev <ogordev@proton.me>
2024-09-22 16:42:37 +0700

    uppdate

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ['vercel.com'], // Add any domains you're loading images from
+    },
+    // Uncomment the following if you're using rewrites or redirects
+    // async rewrites() {
+    //   return []
+    // },
+    // async redirects() {
+    //   return []
+    // },
+    poweredByHeader: false,
+    // Uncomment if you need to use WebAssembly
+    // experimental: { webAssembly: true },
+  };
+  
+  module.exports = nextConfig;
